01a4e9de3db784f3f9d69f5ebdfcdf249912899e
Sergey Meshchankin <smeshchankin@gmail.com>
2021-03-07 08:55:11 +0200

    [MSS-001] Department: Change 404

--- department/src/main/java/com/ss/microservice/department/controller/DepartmentController.java
+++ department/src/main/java/com/ss/microservice/department/controller/DepartmentController.java
@@ -1,16 +1,16 @@
 package com.ss.microservice.department.controller;
 
 import com.ss.microservice.department.entity.Department;
-import com.ss.microservice.department.exception.EntityNotFoundException;
 import com.ss.microservice.department.service.DepartmentService;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -31,7 +31,9 @@ public class DepartmentController {
   @GetMapping("/{id}")
   public Department readOne(@PathVariable Long id) {
     log.info("Read department by id = " + id);
-    Optional<Department> optional = service.getById(id);
-    return optional.orElseThrow(() -> new EntityNotFoundException("Department: " + id));
+    return service.getById(id)
+      .orElseThrow(() -> new ResponseStatusException(
+        HttpStatus.NOT_FOUND, "Department not found with id = " + id
+    ));
   }
 }
