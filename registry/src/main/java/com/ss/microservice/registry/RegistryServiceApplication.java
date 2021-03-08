4dea3b40180eaee268cfb5b25a8b26cebff01640
Sergey Meshchankin <smeshchankin@gmail.com>
2021-03-08 23:16:56 +0200

    [MSS-003] Registry: Init structure (Eureka)

---
+++ registry/src/main/java/com/ss/microservice/registry/RegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.microservice.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
