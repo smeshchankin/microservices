ec2b1267f2b90e90558cab5bbd3fd39f7ecc679a
Sergey Meshchankin <smeshchankin@gmail.com>
2021-03-10 23:51:13 +0200

    [MSS-007] Config Server: Init structure

---
+++ config/src/test/java/com/ss/microservice/config/ConfigApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ss.microservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
